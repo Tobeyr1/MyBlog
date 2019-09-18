7fa7c4403c2ae7a4057aa975eede812887a484c4
Tobey-r1 <13115231059@163.com>
2019-09-18 17:31:15 +0800

    使用高德定位以及和风来展示实况天气

--- src/main/java/com/example/frametest/MainActivity.java
+++ src/main/java/com/example/frametest/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.frametest;
 
 import android.annotation.SuppressLint;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
@@ -30,6 +29,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.afollestad.materialdialogs.MaterialDialog;
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 import com.bumptech.glide.Glide;
 import com.cazaea.sweetalert.SweetAlertDialog;
 import com.example.frametest.UserMode.LoginActivity;
@@ -43,6 +46,7 @@ import com.example.frametest.tools.DBOpenHelper;
 import com.example.frametest.tools.MyApplication;
 import com.google.gson.Gson;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -66,6 +70,9 @@ import interfaces.heweather.com.interfacesmodule.bean.Unit;
 import interfaces.heweather.com.interfacesmodule.bean.air.now.AirNow;
 import interfaces.heweather.com.interfacesmodule.bean.weather.now.Now;
 import interfaces.heweather.com.interfacesmodule.view.HeWeather;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends BasicActivity {
     private android.support.v7.widget.Toolbar toolbar;
@@ -82,6 +89,11 @@ public class MainActivity extends BasicActivity {
     private static boolean mBackKeyPressed = false;//记录是否有首次按键
     private TextView tv_tianqi,tv_kongqi,tv_airqlty;
     ImageView image_weather,image_exit;
+    public AMapLocationClient mLocationClient=null;
+    //声明定位回调监听器
+    public AMapLocationClientOption mLocationOption=null;
+
+    private   String CityId;
     @SuppressLint("HandlerLeak")
     private Handler userFeedHandler = new Handler(){
         @Override
@@ -109,6 +121,7 @@ public class MainActivity extends BasicActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initMap();
         toolbar =  findViewById(R.id.toolbar);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout); //获取抽屉布局
         navigationView = (NavigationView) findViewById(R.id.nav_design);//获取菜单控件实例
@@ -124,10 +137,188 @@ public class MainActivity extends BasicActivity {
          tv_airqlty =(TextView) findViewById(R.id.tv_airqlty);
     }
 
+    private void initMap() {
+        //初始化定位
+        mLocationClient=new AMapLocationClient(MainActivity.this);
+        //设置定位回调监听
+        mLocationClient.setLocationListener(mLocationListener);
+        mLocationOption = new AMapLocationClientOption();
+//设置定位模式为高精度模式，AMapLocationMode.Battery_Saving为低功耗模式，AMapLocationMode.Device_Sensors是仅设备模式
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        mLocationOption.setNeedAddress(true);//设置是否返回地址信息（默认返回地址信息）
+        mLocationOption.setOnceLocation(false);//设置是否只定位一次,默认为false
+        mLocationOption.setWifiActiveScan(true);//设置是否强制刷新WIFI，默认为强制刷新
+        mLocationOption.setMockEnable(false);//设置是否允许模拟位置,默认为false，不允许模拟位置
+        mLocationOption.setInterval(15000);//设置定位间隔,单位毫秒,默认为2000ms
+        mLocationOption.setOnceLocation(false);//可选，是否设置单次定位默认为false即持续定位
+        mLocationOption.setOnceLocationLatest(false); //可选，设置是否等待wifi刷新，默认为false.如果设置为true,会自动变为单次定位，持续定位时不要使用
+        mLocationOption.setWifiScan(true); //可选，设置是否开启wifi扫描。默认为true，如果设置为false会同时停止主动刷新，停止以后完全依赖于系统刷新，定位位置可能存在误差
+        mLocationOption.setLocationCacheEnable(true); //可选，设置是否使用缓存定位，默认为true
+//给定位客户端对象设置定位参数
+        mLocationClient.setLocationOption(mLocationOption);
+//启动定位
+        mLocationClient.startLocation();
+    }
+    public AMapLocationListener mLocationListener=new AMapLocationListener() {
+        @Override
+        public void onLocationChanged(AMapLocation aMapLocation) {
+            if (aMapLocation != null) {
+                if (aMapLocation.getErrorCode() == 0) {
+                    //定位成功回调信息，设置相关消息
+                    aMapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见定位类型表
+                    // aMapLocation.getLatitude();//获取纬度
+                    // aMapLocation.getLongitude();//获取经度
+                    aMapLocation.getAccuracy();//获取精度信息
+                    java.text.SimpleDateFormat df = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+                    //  aMapLocation.getAddress();//地址，如果option中设置isNeedAddress为false，则没有此结果，网络定位结果中会有地址信息，GPS定位不返回地址信息。
+                    //  aMapLocation.getCountry();//国家信息
+                    //  aMapLocation.getProvince();//省信息
+                    //  aMapLocation.getCity();//城市信息
+                    //   aMapLocation.getDistrict();//城区信息
+                    //    aMapLocation.getStreet();//街道信息
+                    //     aMapLocation.getStreetNum();//街道门牌号信息
+                    //    aMapLocation.getCityCode();//城市编码
+                    //     aMapLocation.getAdCode();//地区编码
+                    //获取经纬度
+                    double  LongitudeId = aMapLocation.getLongitude();
+                    double LatitudeId = aMapLocation.getLatitude();
+                    //获取定位城市定位的ID
+                    requestCityInfo(LongitudeId,LatitudeId);
+                    mLocationClient.stopLocation();//停止定位
+                } else {
+                    //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
+                    Log.e("info", "location Error, ErrCode:"
+                            + aMapLocation.getErrorCode() + ", errInfo:"
+                            + aMapLocation.getErrorInfo());
+                }
+            }
+        }
+    };
+    public void  requestCityInfo(double longitude,double latitude){
+        //这里的key是webapi key
+        String cityUrl = "https://search.heweather.net/find?location="+longitude+","+latitude+"&key=6529091f5dc44fbc94d900dc2ca67e96";
+        sendRequestWithOkHttp(cityUrl);
+    }
+    //解析根据经纬度获取到的含有城市id的json数据
+    private void sendRequestWithOkHttp(String cityUrl) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request request = new Request.Builder().url(cityUrl).build();
+                try {
+                    Response response = okHttpClient.newCall(request).execute();
+                    //返回城市列表json数据
+                    String responseData = response.body().string();
+                    System.out.println("变成json数据的格式："+responseData);
+                    JSONObject jsonWeather = null;
+                    try {
+                        jsonWeather = new JSONObject(responseData);
+                        JSONArray jsonArray = jsonWeather.getJSONArray("HeWeather6");
+                        JSONObject jsonObject = jsonArray.getJSONObject(0);
+                        String jsonStatus = jsonObject.getString("status");
+                        if (jsonStatus.equals("ok")){
+                            JSONArray jsonBasic = jsonObject.getJSONArray("basic");
+                            JSONObject jsonCityId = jsonBasic.getJSONObject(0);
+                            CityId = jsonCityId.getString("cid");
+                            getWether();
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void getWether() {
+        /**
+         * 实况天气
+         * 实况天气即为当前时间点的天气状况以及温湿风压等气象指数，具体包含的数据：体感温度、
+         * 实测温度、天气状况、风力、风速、风向、相对湿度、大气压强、降水量、能见度等。
+         *
+         * @param context  上下文
+         * @param location 地址详解
+         * @param lang       多语言，默认为简体中文
+         * @param unit        单位选择，公制（m）或英制（i），默认为公制单位
+         * @param listener  网络访问回调接口
+         */
+        HeWeather.getWeatherNow(MainActivity.this, CityId,  Lang.CHINESE_SIMPLIFIED , Unit.METRIC , new HeWeather.OnResultWeatherNowBeanListener() {
+            public static final String TAG="he_feng_now";
+            @Override
+            public void onError(Throwable e) {
+                Log.i(TAG, "onError: ", e);
+                System.out.println("Weather Now Error:"+new Gson());
+            }
+
+            @Override
+            public void onSuccess(Now dataObject) {
+                Log.i(TAG, " Weather Now onSuccess: " + new Gson().toJson(dataObject));
+                String jsonData = new Gson().toJson(dataObject);
+                System.out.println("返回的数据内容："+dataObject.getStatus());
+                String tianqi = null,wendu = null, tianqicode = null;
+                if (dataObject.getStatus().equals("ok")){
+                    String JsonNow = new Gson().toJson(dataObject.getNow());
+                    JSONObject jsonObject = null;
+                    try {
+                        jsonObject = new JSONObject(JsonNow);
+                        tianqi = jsonObject.getString("cond_txt");
+                        wendu = jsonObject.getString("tmp");
+                        tianqicode = jsonObject.getString("cond_code");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }else {
+                    Toast.makeText(MainActivity.this,"有错误",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                String wendu2 = wendu +"℃";
+                tv_tianqi.setText(tianqi);
+                tv_kongqi.setText(wendu2);
+                String tagurl = "https://cdn.heweather.com/cond_icon/" +tianqicode+".png";
+                Glide.with(MainActivity.this).load(tagurl).into(image_weather);
+            }
+        });
+        HeWeather.getAirNow(MainActivity.this, CityId, Lang.CHINESE_SIMPLIFIED, Unit.METRIC, new HeWeather.OnResultAirNowBeansListener() {
+            public static final String TAG2="he_feng_air";
+            @Override
+            public void onError(Throwable throwable) {
+                Log.i(TAG2,"ERROR IS:",throwable);
+            }
+
+            @Override
+            public void onSuccess(AirNow airNow) {
+                Log.i(TAG2,"Air Now onSuccess:"+new Gson().toJson(airNow));
+                String airStatus = airNow.getStatus();
+                if (airStatus.equals("ok")){
+                    String jsonData = new Gson().toJson(airNow.getAir_now_city());
+                    String aqi = null,qlty = null;
+                    JSONObject objectAir = null;
+                    try {
+                        objectAir = new JSONObject(jsonData);
+                        aqi = objectAir.getString("aqi");
+                        qlty = objectAir.getString("qlty");
+                        tv_airqlty.setText(qlty+"("+aqi+")");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }else {
+                    Toast.makeText(MainActivity.this,"有错误",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+
+        });
+
+    }
+
+
     @Override
     protected void onStart() {
         super.onStart();
-        getWeather();
        /* toolbar.setLogo(R.drawable.icon);//设置图片logo,你可以添加自己的图片*/
         toolbar.setTitle("简易新闻");
         setSupportActionBar(toolbar);
@@ -267,86 +458,6 @@ public class MainActivity extends BasicActivity {
 
     }
 
-    private void getWeather() {
-        /**
-         * 实况天气
-         * 实况天气即为当前时间点的天气状况以及温湿风压等气象指数，具体包含的数据：体感温度、
-         * 实测温度、天气状况、风力、风速、风向、相对湿度、大气压强、降水量、能见度等。
-         *
-         * @param context  上下文
-         * @param location 地址详解
-         * @param lang       多语言，默认为简体中文
-         * @param unit        单位选择，公制（m）或英制（i），默认为公制单位
-         * @param listener  网络访问回调接口
-         */
-        HeWeather.getWeatherNow(MainActivity.this, "CN101190101",  Lang.CHINESE_SIMPLIFIED , Unit.METRIC , new HeWeather.OnResultWeatherNowBeanListener() {
-            public static final String TAG="he_feng_now";
-            @Override
-            public void onError(Throwable e) {
-                Log.i(TAG, "onError: ", e);
-                System.out.println("Weather Now Error:"+new Gson());
-            }
-
-            @Override
-            public void onSuccess(Now dataObject) {
-                Log.i(TAG, " Weather Now onSuccess: " + new Gson().toJson(dataObject));
-                String jsonData = new Gson().toJson(dataObject);
-                System.out.println("返回的数据内容："+dataObject.getStatus());
-                String tianqi = null,wendu = null, tianqicode = null;
-                if (dataObject.getStatus().equals("ok")){
-                    String JsonNow = new Gson().toJson(dataObject.getNow());
-                    JSONObject jsonObject = null;
-                    try {
-                        jsonObject = new JSONObject(JsonNow);
-                        tianqi = jsonObject.getString("cond_txt");
-                        wendu = jsonObject.getString("tmp");
-                        tianqicode = jsonObject.getString("cond_code");
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }else {
-                    Toast.makeText(MainActivity.this,"有错误",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                String wendu2 = wendu +"℃";
-                tv_tianqi.setText(tianqi);
-                tv_kongqi.setText(wendu2);
-                String tagurl = "https://cdn.heweather.com/cond_icon/" +tianqicode+".png";
-                Glide.with(MainActivity.this).load(tagurl).into(image_weather);
-            }
-        });
-        HeWeather.getAirNow(MainActivity.this, "CN101190101", Lang.CHINESE_SIMPLIFIED, Unit.METRIC, new HeWeather.OnResultAirNowBeansListener() {
-            public static final String TAG2="he_feng_air";
-            @Override
-            public void onError(Throwable throwable) {
-                Log.i(TAG2,"ERROR IS:",throwable);
-            }
-
-            @Override
-            public void onSuccess(AirNow airNow) {
-                Log.i(TAG2,"Air Now onSuccess:"+new Gson().toJson(airNow));
-                String airStatus = airNow.getStatus();
-                if (airStatus.equals("ok")){
-                    String jsonData = new Gson().toJson(airNow.getAir_now_city());
-                    String aqi = null,qlty = null;
-                    JSONObject objectAir = null;
-                    try {
-                        objectAir = new JSONObject(jsonData);
-                        aqi = objectAir.getString("aqi");
-                        qlty = objectAir.getString("qlty");
-                        tv_airqlty.setText(qlty+"("+aqi+")");
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }else {
-                    Toast.makeText(MainActivity.this,"有错误",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-            }
-
-        });
-
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
